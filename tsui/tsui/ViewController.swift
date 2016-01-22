b6d1c94a6bd6c35b6b8ae2bec76d2150ace5f0c9
徐宁 <xuning@tsuios.com>
2016-01-22 15:05:10 +0800

    显示基本数据

--- tsui/tsui/ViewController.swift
+++ tsui/tsui/ViewController.swift
@@ -9,19 +9,41 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    
+    //个人信息数组
+    private var personInfo: [Person]?
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadData()
+        loadData { (array) -> () in
+            //接收回调参数
+            self.personInfo = array
+            //刷新表格
+            self.tableView.reloadData()
+        }
     }
 
+
+}
+// MARK: - 数据源方法
+extension ViewController {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //如果personInfo 数组为nil 直接返回0
+        return personInfo?.count ?? 0
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = personInfo![indexPath.row].name
+        return cell
+    }
 
 }
 
 // MARk: - 拼接个人信息
 extension ViewController {
     
-    private func loadData() {
+    private func loadData(completion: (array: [Person]) -> ()) {
     
         dispatch_async(dispatch_get_global_queue(0, 0)) { () -> Void in
             //模拟耗时操作
@@ -42,11 +64,12 @@ extension ViewController {
             
             }
             //测试数据
-            print(dataList)
+//            print(dataList)
             
             dispatch_async(dispatch_get_main_queue()) { () -> Void in
                 //完成回调
                 print("完成回调")
+                completion(array: dataList)
             }
         }
     }
